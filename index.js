87111c2d2f66813b3478d0ad0531eea333afb60f
Vando Pinto <vandopintoo@gmail.com>
2026-04-10 11:45:46 -0300

    link

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ app.post('/upload', upload.single('arquivo'), async (req, res) => {
     // ✅ Retorna link correto
     res.send(`
       Arquivo processado com sucesso! <br><br>
-      <a href="./output/licao/topico1/tela1.html" target="_blank">
+      <a href="./automato/aulafuncional/index.html" target="_blank">
         Ver arquivo gerado
       </a>
   `);
@@ -71,59 +71,3 @@ app.post('/upload', upload.single('arquivo'), async (req, res) => {
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
 });
-
-
-// // ================== PROCESSAMENTO ==================
-
-// const { CarregarArquivoDOCX } = require("./automato/scripts/CarregarArquivoDOCX");
-// const { CriarCabecalho } = require("./automato/scripts/CriarCabecalho");
-// const { SelecionarTelas } = require("./automato/scripts/SelecionarTelas");
-// const { parse } = require("node-html-parser");
-
-// // Caminho base de saída
-// const caminhoBaseSaida = path.resolve(__dirname, "./output");
-
-// async function executar(arquivo) {
-//   let tabelas = [];
-
-//   try {
-//     const html = await CarregarArquivoDOCX(arquivo, caminhoBaseSaida);
-
-//     const root = parse(html);
-
-//     const tables = Array.from(root.children)
-//       .filter(el => el.tagName === "TABLE");
-
-//     let ultimoArquivoGerado = null;
-
-//     tables.forEach((tabela, index) => {
-//       tabelas.push(tabela.innerHTML);
-
-//       if (index === 0) {
-//         CriarCabecalho(parse(tabela.innerHTML));
-//       } else {
-//         // 🔥 IMPORTANTE:
-//         // Ajuste sua função SelecionarTelas para retornar o caminho do arquivo gerado
-//         const caminhoGerado = SelecionarTelas(
-//           parse(tabela.innerHTML),
-//           index,
-//           caminhoBaseSaida
-//         );
-
-//         // guarda o último (ou você pode guardar todos depois)
-//         if (caminhoGerado) {
-//           ultimoArquivoGerado = caminhoGerado;
-//         }
-//       }
-//     });
-
-//     console.log("TABELAS ENCONTRADAS:", tabelas.length);
-
-//     // ✅ retorno dinâmico
-//     return ultimoArquivoGerado || 'licao/topico1/tela22.html';
-
-//   } catch (erro) {
-//     console.error("❌ Erro:", erro);
-//     throw erro;
-//   }
-// }
